ce8a60c467c7237accda3c2a74c1fb8d83a75eec
Simon35845 <DrakeKingman1991@gmail.com>
2025-01-13 00:10:16 +0300

    commit 2.6 - пофиксил HibernateUtil, теперь данные корректно обновляются

--- src/main/java/createdBy51mon/utils/HibernateUtil.java
+++ src/main/java/createdBy51mon/utils/HibernateUtil.java
@@ -7,12 +7,16 @@ import javax.persistence.Persistence;
 public class HibernateUtil {
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY =
             Persistence.createEntityManagerFactory("persistence_by_51mon");
+    public static final EntityManager ENTITY_MANAGER =
+            ENTITY_MANAGER_FACTORY.createEntityManager();
 
     public static EntityManager getEntityManager() {
-        return ENTITY_MANAGER_FACTORY.createEntityManager();
+        return ENTITY_MANAGER;
     }
 
     public static void close() {
-        ENTITY_MANAGER_FACTORY.close();
+        if (ENTITY_MANAGER_FACTORY.isOpen()) {
+            ENTITY_MANAGER_FACTORY.close();
+        }
     }
 }
